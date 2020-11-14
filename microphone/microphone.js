2726911e865a027e9fa034aca42c1202b74361be
enrico-zoboli <74058930+enrico-zoboli@users.noreply.github.com>
2020-11-14 14:31:44 +0100

    mic&server

---
+++ microphone/microphone.js
@@ -0,0 +1,81 @@
+c = new AudioContext()
+canvas = document.querySelector("canvas")
+ctx = canvas.getContext("2d")
+a = c.createAnalyser()
+
+//o.connect(a);
+//o.start();
+var data = new Uint8Array(a.frequencyBinCount)
+
+var typed_array = [];
+
+var arr_to_send = [];
+
+stop = false;
+
+p = navigator.mediaDevices.getUserMedia({ audio: true })
+var ms
+p.then(function (_ms) {
+    ms = _ms
+    var mss
+    mss = c.createMediaStreamSource(ms)
+    mss.connect(a)
+})
+
+function drawAnalyzer() {
+    requestAnimationFrame(drawAnalyzer);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    a.getByteTimeDomainData(data);
+    //a.getByteFrequencyData(data);
+    ctx.beginPath();
+    for (i = 0; i < data.length; i++) {
+        ctx.lineTo(i, data[i])
+    }
+    ctx.stroke()
+}
+
+var start;
+
+millsec_to_call = (1024/48000)*1000
+
+
+
+function startreconding() {
+
+    typed_array=[];
+    start_timer();
+    start = setInterval(stock_data, millsec_to_call)
+    //extract_data(data);
+}
+
+function stoprecording() {
+    clearInterval(start)
+
+    post_to_server(typed_array)
+}
+
+
+drawAnalyzer();
+
+
+function stock_data() {
+    console.log('ciao')
+
+    for (var i = 0; i < data.length; i++) {
+        typed_array.push(data[i])
+    }
+}
+
+
+//setInterval(drawAnalyzer,100)
+
+
+let startbutton = document.getElementById("start-rec");
+let stopbutton = document.getElementById("stop-rec");
+
+//console.log(data);
+startbutton.onclick = startreconding;
+stopbutton.onclick = stoprecording;
+
+//document.querySelector("button").onclick = function(){c.resume()}
+
---
+++ microphone/timer.js
@@ -0,0 +1,41 @@
+//var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+/*var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+
+    seconds = now.seconds;
+    minutes = now.minutes;
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+    // If the count down is over, write some text 
+}, 1000);*/
+
+//setInterval(get_millseconds, 1)
+
+function start_timer() {
+    setInterval(get_seconds, 1000)
+}
+var seconds = 0
+var millsec = 0
+
+function get_millseconds() {
+    if (millsec == 1000) {
+        millsec = 0
+    }
+
+    document.getElementById("millsec").innerHTML = millsec + "ms";
+    millsec++
+}
+
+function get_seconds() {
+
+
+    document.getElementById("seconds").innerHTML = seconds + "s";
+    seconds++
+}
